47e6df55b4415ad2f23f01ab6e12a9377d6f1851
Tran Dinh Tu <d_tu@brycen.com.vn>
2024-05-23 09:30:57 +0700

    updated

--- jest.config.js
+++ jest.config.js
@@ -13,3 +13,10 @@ const customJestConfig = {
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
+module.exports = {
+  coverageThreshold: {
+    global: {
+      lines: 80,
+    },
+  },
+};
